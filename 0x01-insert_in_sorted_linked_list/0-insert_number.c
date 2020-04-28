7c472d4e7c734f87f036a80a8d4dd85e7b688277
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-04-28 06:56:51 +0000

    insert a number in middle of list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert a number into a sorted linked list
+ * @number: integer within the node
+ * @head: double pointer pointing to the first node
+ *
+ * Return: the address of the new node, or Null if failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *ptr;
+	listint_t *prev = NULL;
+	listint_t *new_node;
+
+
+	ptr = *head;
+	if (ptr == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+
+	if (number == ptr->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+
+	/*new_node->next = NULL;*/
+
+	for (; ptr != NULL && ptr->n < number; ptr = ptr->next)
+		prev = ptr;
+	new_node->next = ptr;
+	prev->next = new_node;
+	return (new_node);
+}
