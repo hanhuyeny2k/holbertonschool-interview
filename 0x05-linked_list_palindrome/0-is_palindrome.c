fce6ef86d0b3c161c4bd234f92b3180ee6e0e7af
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-05-28 02:24:26 +0000

    edited edge cases with return 1

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -34,18 +34,19 @@ int compareLists(listint_t *head1, listint_t *head2)
 	listint_t *temp1 = head1;
 	listint_t *temp2 = head2;
 
+	if (temp1 == NULL && temp2 == NULL)
+		return (1);
 	while (temp1 && temp2)
 	{
 		if (temp1->n == temp2->n)
 		{
 			temp1 = temp1->next;
 			temp2 = temp2->next;
+			return (1);
 		}
 		else
 			return (0);
 	}
-	if (temp1 == NULL && temp2 == NULL)
-		return (1);
 	return (0);
 }
 
@@ -62,6 +63,8 @@ int is_palindrome(listint_t **head)
 	listint_t *midnode = NULL;
 	int result;
 
+	if (head == NULL)
+		return (1);
 	if (head != NULL && (*head)->next != NULL)
 	{
 		while (fast_ptr != NULL && fast_ptr->next != NULL)
@@ -86,7 +89,7 @@ int is_palindrome(listint_t **head)
 			midnode->next = second_half;
 		}
 		else
-			prev_of_slow_ptr = second_half;
+			prev_of_slow_ptr->next = second_half;
 	}
 	return (result);
 }
