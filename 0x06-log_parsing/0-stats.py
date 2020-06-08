5c3f98cda1576961152b4039faa83f3388b7e12c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-06-08 20:47:24 +0000

    log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+
+file_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def print_stats(file_size, status_codes):
+    """Print log statistics"""
+    print('File size: {}'.format(file_size))
+    for key in sorted(status_codes):
+        if status_codes[key] > 0:
+            print('{}: {}'.format(key, status_codes[key]))
+
+
+try:
+    for linenum, line in enumerate(sys.stdin, 1):
+        fields = line.split()
+        if len(fields) < 6:
+            pass
+        file_size += int(fields.pop())
+        status_codes[int(fields.pop())] += 1
+        if linenum % 10 == 0:
+            print_stats(file_size, status_codes)
+except KeyboardInterrupt:
+    print_stats(file_size, status_codes)
+    raise
