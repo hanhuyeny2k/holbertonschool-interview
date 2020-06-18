bbf3dd8ee03ced9d607f621e28de85f9de5d58e4
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-06-18 03:00:18 +0000

    header file

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,16 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+#include <stdlib.h>
+
+/**
+ * is_palindrome - check if a given int a palindrome
+ * @n: integer
+ *
+ * return: 1 if n is a palindrome, and 0 otherwise
+ * Description: Palindrome unsigned integer
+ * for Holberton project
+ */
+int is_palindrome(unsigned long n);
+
+#endif /* palindrome.h */
