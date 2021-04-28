c7159a168ba04699bfb2110ccd0b6458357b8c73
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-04-28 18:45:48 +0000

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''
-Given a set of consecutive integers starting from 1 up to and including n, the players take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game.
+Given a set of consecutive integers starting from 1 up to and including n, the
+players take turns choosing a prime number from the set and removing that
+number and its multiples from the set. The player that cannot make a move loses
+the game.
 '''
 
 
